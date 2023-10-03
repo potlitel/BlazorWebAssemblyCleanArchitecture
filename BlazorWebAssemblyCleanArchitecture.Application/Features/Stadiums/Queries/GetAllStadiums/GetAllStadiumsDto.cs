ccd630ba770c78cb7b4174fddb165394c61d857b
potlitel <potlitel@gmail.com>
2023-10-03 18:14:39 -0400

    Se completan las capas de Aplicación y Dominio.

---
+++ BlazorWebAssemblyCleanArchitecture.Application/Features/Stadiums/Queries/GetAllStadiums/GetAllStadiumsDto.cs
@@ -0,0 +1,16 @@
+﻿using BlazorWebAssemblyCleanArchitecture.Application.Common.Mappings;
+using BlazorWebAssemblyCleanArchitecture.Domain.Entities;
+
+namespace BlazorWebAssemblyCleanArchitecture.Application.Features.Stadiums.Queries.GetAllStadiums
+{
+    public class GetAllStadiumsDto : IMapFrom<Stadium>
+    {
+        public int Id { get; init; }
+        public string Name { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public int? Capacity { get; set; }
+        public int? BuiltYear { get; set; }
+        public int? PitchLength { get; set; }
+        public int? PitchWidth { get; set; }
+    }
+}
---
+++ BlazorWebAssemblyCleanArchitecture.Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using BlazorWebAssemblyCleanArchitecture.Domain.Common.Interfaces;
+
+namespace BlazorWebAssemblyCleanArchitecture.Domain.Common
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
